45c4dae2cd610cf2d77deabff7b9545d66396d2f
Farhan-Kebab <112526215+Farhan-Kebab@users.noreply.github.com>
2022-09-07 08:44:26 +0700

    Add files via upload

---
+++ python7-TKJ4.py
@@ -0,0 +1,14 @@
+list_kosong = []
+list_integer = [1, 2, 3,10,100 ,250]
+list_string = ["ambatukam", "farhan kebab", "reza kecap", "fu'ad racing mandalika"]
+list_mix = [20, "ambatukam", True, "niger"]
+
+print("list kosong:", list_kosong)
+print("list integer:", list_integer)
+print("list string:", list_string)
+print("list mix:", list_mix)
+
+print("list data kosong:", list_kosong)
+print("list data integer pertama:", list_integer[0])
+print("list data string kedua:", list_string[3])
+print("list data mix ketiga:", list_mix[1])
