3332d8a48830d7e0f40cfc68c93dec62f4f3a3a5
Farhan-Kebab <112526215+Farhan-Kebab@users.noreply.github.com>
2022-09-09 14:38:26 +0700

    Add files via upload

---
+++ UPRAK_Rafi Afrand_XTKJ4.py
@@ -0,0 +1,34 @@
+# volume balok
+p = 12
+l = 24
+t = 6
+jumlah_plt = 12 * 24 * 6
+print("Jumlah volume balok adalah", jumlah_plt)
+
+# Volume kubus
+s1 = 4
+s2 = 4
+s3 = 4
+jumlah_sss = 4 * 4 * 4
+print("Jumlah volume kubus adalah", jumlah_sss)
+
+# Volume limas
+p = 0.3
+la = 34
+t = 45
+jumlah_plat = 0.3 * 34 * 45
+print("Jumlah volume limas adalah", jumlah_plat)
+
+# Volume tabung
+phi = 22/7
+r = 4
+r = 4
+t = 6
+jumlah_prrt = 22/7 * 4 * 4 * 6
+print("Jumlah volume tabung adalah", jumlah_prrt)
+
+# ubah celcius ke reamur
+c = 4
+r = 5
+jumlah_ckr = 4/5 * 15
+print("Jumlah celcius ke reamur adalah", jumlah_ckr)
