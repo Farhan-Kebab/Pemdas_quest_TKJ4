568ba6c26d28dda5b03a560256b3ee9f95b8a2fd
Farhan-Kebab <112526215+Farhan-Kebab@users.noreply.github.com>
2022-10-10 09:43:10 +0700

    Add files via upload

---
+++ python10-TKJ4.py
@@ -0,0 +1,27 @@
+# Membuat Tipe Data List
+
+koleksi_data_str = ["Pisang", "Pangga", "Jambu", "Semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["Rizky Billar", 100, True, "Reza Arap"]
+
+print("koleksi data string:", koleksi_data_str)
+
+print("koleksi data integer:", koleksi_data_int)
+
+print("koleksi data campuran:", koleksi_data_mix)
+
+# buatlah 3 kumpulan data: Nama hewan, Nilai UTS, Nama teman sebangku kalian
+
+list_nama_hewan = ["anjing", "babi", "elang"]
+
+list_nilai_uts = [76, 50, 45,]
+
+list_teman_sebangku = ["ilal aseli", "fajri roblox", "rifqi cepot", "angga sepuh kapal"]
+
+print("list Nama Hewan:", list_nama_hewan)
+
+print("List Nilai UTS:", list_nilai_uts)
+
+print("List Nama Teman Sebangku:", list_teman_sebangku)
