17122c37e61e1c664786bfe1b20e5eec84caf6b7
Farhan-Kebab <112526215+Farhan-Kebab@users.noreply.github.com>
2022-10-13 08:04:18 +0700

    Add files via upload

--- python10-TKJ4.py
+++ python10-TKJ4.py
@@ -25,3 +25,24 @@ print("list Nama Hewan:", list_nama_hewan)
 print("List Nilai UTS:", list_nilai_uts)
 
 print("List Nama Teman Sebangku:", list_teman_sebangku)
+
+# data ke-2 nama hewan
+print("data ke-2 nama hewan:", list_nama_hewan[1])
+
+# data ke-3 nilai uts
+print('data ke-3 nilai uts:', list_nilai_uts[2])
+
+# data ke-1 nama teman sebangku
+print('data ke-1 nama teman sebangku:', list_teman_sebangku[1])
+
+# tambahkan 1 data disetiap data koleksi
+
+list_nama_hewan.append('icikiwir')
+list_nilai_uts.append('93')
+list_teman_sebangku.append('bandar apran')
+
+# ubahlah data ke-3 nama hewan
+
+list_nama_hewan[2] = 'penguin'
+print(list_nama_hewan)
+
