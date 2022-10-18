bb0d71ed01645780f4f8e28b1a9a17f006a269db
Farhan-Kebab <112526215+Farhan-Kebab@users.noreply.github.com>
2022-10-18 09:43:55 +0700

    Add files via upload

---
+++ python11-TKJ4.py
@@ -0,0 +1,23 @@
+gender = ("laki-laki", "perempun")
+
+# menggunakan fungsi Tuple,tuple()
+
+tuple_name = tuple(["ilal aseli", "rifqi sang raja hutan", "fajri predator", "angga enterprise"])
+
+gabungan = gender + tuple_name
+
+print(gabungan)
+
+# buatlah 3 koleksi tuple dengan minimal 4 data di setiap koleksi
+
+nama = ("icikiwir","aselole","slamet kopling","rifqi sang raja hutan")
+
+hewan = ("beruang","singa","anjing","babi hutan")
+
+top_5_game = ("genshit infek","ajurlen","arsip biru","mobil lejen","hongkai impek")
+
+print(nama)
+
+print(hewan)
+
+print(top_5_game)
